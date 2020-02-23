6ceb4211c14e4ea7e87a95c89cfe1c978d0ad6d8
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-23 16:46:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    each_element = array[i] # <== Unique work
+    new.push(yield(each_element))
+    i += 1
+  end
+  new
+end
+
+def reduce(array, starting_point = 0)
+index = 0
+total = starting_point
+while index < array.length do
+  total = yield(total + array[index])
+  index += 1
+end
+total
+end
