ab7c8a06179dd704bc8de6742ff1e3e60fa800e4
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-23 17:00:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,14 +13,14 @@ end
 def reduce(array, starting_point=nil)
 if starting_point
   total = starting_point
-  index = 0
+  i = 0
 else
   total = array[0]
-  index = 1
+  i = 1
 end
-while index < array.length do
-  total = yield(total + array[index])
-  index += 1
+while i < array.length do
+  total = yield(total + array[i])
+  i += 1
 end
 total
 end
